201aabb5ca922483ed9184b2f6a0971ab9f26353
Bohdan Hishka <reasonx7@gmail.com>
2019-12-19 17:52:24 +0200

    MPromise

--- src/MPromise.js
+++ src/MPromise.js
@@ -1,7 +1,29 @@
-export default class MPromise {
-  constructor(callback) {}
+class MPromise {
+  constructor(callback) {
+    setTimeout(() => {
+      let result = undefined;
+      const onResolve = this.onResolve;
 
-  then(onResolve, onReject) {}
+      callback((res) => {
+        result = res;
+        onResolve && onResolve(result);
+      });
+
+      setTimeout(() => {
+        this.nextResolve && this.nextResolve(result);
+      });
+    });
+  }
+
+  then(onResolve) {
+    this.onResolve = onResolve;
+
+    return new MPromise((resolve) => {
+      this.nextResolve = resolve;
+    });
+  }
 
   catch(onReject) {}
 }
+
+module.exports = MPromise;
--- src/MPromise.spec.js
+++ src/MPromise.spec.js
@@ -1,21 +1,39 @@
-import MPromise from "./MPromise";
+const MPromise = require('./MPromise');
 
 describe("Promise", () => {
   jest.useFakeTimers();
 
-  it("should resolve a Promise", () => {
+  it("should resolve a Promise", (done) => {
     expect.assertions(1);
 
     const promise = new MPromise(resolve => {
       setTimeout(() => resolve(Math.PI), 1000);
     });
 
-    promise.then(result => expect(result).toBe(Math.PI));
+    promise.then(result => {
+      expect(result).toBe(Math.PI);
+      done();
+    });
 
     jest.advanceTimersByTime(1000);
   });
 
-  it("should resolve transform data via then chain", () => {});
+  it("should resolve transform data via then chain", (done) => {
+    expect.assertions(1);
 
-  it("should catch an error", () => {});
+    const promise = new MPromise(resolve => {
+      setTimeout(() => resolve(1), 1000);
+    });
+
+    promise
+      .then(result => result + 2)
+      .then(result => result + 3)
+      .then(result => result + 4)
+      .then((result) => {
+        expect(result).toBe(10);
+        done();
+      });
+
+    jest.advanceTimersByTime(1000);
+  });
 });
