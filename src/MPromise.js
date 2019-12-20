7a20d1d1eec21fc1570f27c0c9edfb37a6e1f749
Bohdan Hishka <reasonx7@gmail.com>
2019-12-20 12:14:34 +0200

    Remove unused code

--- src/MPromise.js
+++ src/MPromise.js
@@ -1,26 +1,8 @@
 class MPromise {
   constructor(callback) {
-    setTimeout(() => {
-      let result = undefined;
-      const onResolve = this.onResolve;
-
-      callback((res) => {
-        result = res;
-        onResolve && onResolve(result);
-      });
-
-      setTimeout(() => {
-        this.nextResolve && this.nextResolve(result);
-      });
-    });
   }
 
   then(onResolve) {
-    this.onResolve = onResolve;
-
-    return new MPromise((resolve) => {
-      this.nextResolve = resolve;
-    });
   }
 
   catch(onReject) {}
